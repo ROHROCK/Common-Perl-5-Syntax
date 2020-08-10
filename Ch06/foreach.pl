07146b9f7c5319925ec9e5f97ac9a946ca7b81e1
Rohit Agharkar <rohitsa40@gmail.com>
2020-08-10 23:14:11 +0530

    updated 6th unit

--- Ch06/foreach.pl
+++ Ch06/foreach.pl
@@ -6,6 +6,12 @@ use warnings;
 
 my @array = qw( one two three four five );
 
-foreach my $s ( @array ) {
-    say $s;
-}
+# Note : foreach and for  ,these both keywords can be used to traverse the list , but to keep it readable follow for - traditional loop and foreach - iterating lists
+
+
+#foreach my $s ( @array ) {
+#    say $s;
+#}
+
+# more cleaner way of writing for each , 
+say foreach @array
--- Ch07/defvar.pl
+++ Ch07/defvar.pl
@@ -5,4 +5,17 @@ use 5.18.0;
 use warnings;
 
 my @array = qw( one two three four five );
-say foreach @array;
+#say foreach @array;
+
+# there is a default variable , it will be clear after looking at the example below
+
+#foreach my $defaultvar (@array){
+#	say $defaultvar
+#}
+
+# Here there is no variable in this for loop , " $_ " is the default variable !
+foreach (@array){
+	say #$_ is the variable
+}
+
+# There will be many functions which will use $_ , you can google it to find more info about it.
